29ad5cac9ec6e67828f5b567d61dc62bf99196eb
taobaibia <48398413+taobaibia@users.noreply.github.com>
2023-04-12 16:21:33 +0800

    Add files via upload

---
+++ elasticsearch-demo-admin-web/src/main/java/com/example/elasticsearchdemoadminweb/ElasticsearchDemoAdminWebApplication.java
@@ -0,0 +1,14 @@
+package com.example.elasticsearchdemoadminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchDemoAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchDemoAdminWebApplication.class, args);
+        System.out.println("==================start====================");
+    }
+
+}
---
+++ elasticsearch-demo-admin-web/src/test/java/com/example/elasticsearchdemoadminweb/ElasticsearchDemoAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchdemoadminweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchDemoAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
